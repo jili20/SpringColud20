010e44ba226305e72cdc7250fe02b89acb08b038
Bing <bing_yu2001@163.com>
2020-05-25 13:11:45 +0800

    初次集群环境构建成功

---
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -0,0 +1,7 @@
+package com.atguigu.springcloud;
+
+/**
+ * @author bing  @create 2020/5/25 11:29 上午
+ */
+public class EurekaMain7001 {
+}
---
+++ cloud-eureka-server7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
@@ -0,0 +1,7 @@
+package com.atguigu.springcloud;
+
+/**
+ * @author bing  @create 2020/5/25 1:02 下午
+ */
+public class EurekaMain7002 {
+}
