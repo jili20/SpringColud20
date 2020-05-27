2547d497db13ad18d38284f8b0db1845863c5721
Bing <bing_yu2001@qq.com>
2020-05-27 20:57:06 +0800

    SpringCloud config通过GitHub获取配置信息

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+/**
+ * @author bing  @create 2020/5/27 8:08 下午
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344
+{
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
