3cb73835aa1a4192b239acbfa91f3218dc6d6738
Bing <bing_yu2001@qq.com>
2020-05-27 22:43:16 +0800

    SpringCloud Config分布式配置中心

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springbloud/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.atguigu.springbloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * @author bing  @create 2020/5/27 9:29 下午
+ */
+@RestController
+@RefreshScope // 具备刷新功能
+public class ConfigClientController
+{
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo()
+    {
+        return configInfo;
+    }
+}
