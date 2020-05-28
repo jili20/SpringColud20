827bcfa8a77d7d6442d737a29084c9ad88df7cad
Bing <bing_yu2001@qq.com>
2020-05-28 15:07:51 +0800

    SpringCloud Bus动态刷新全局广播

---
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * @author bing  @create 2020/5/28 2:05 下午
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController
+{
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String configInfo()
+    {
+        return "serverPort: "+serverPort+"\t\n\n configInfo: "+configInfo;
+    }
+}
